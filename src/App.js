bc093644e310889728a0f14fca41131f306abf80
Administrator adm-virtedu <adm-virtedu@iet-gibb.ch>
2019-08-22 10:46:42 +0200

    Title and logo added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-export default App;
+  showCock = () => {
+    console.log("heilige Maria !!!");
+  }
+  pTest = () => {
+    console.log("works");
+  }
+
+  render() {
+    return(
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p onClick={this.pTest}>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                onClick={this.showCock}
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+    );
+  }
+}
