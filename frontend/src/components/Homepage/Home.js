00e7c4891fc36a137120f3f72f21e7bd81ee020c
Administrator adm-virtedu <adm-virtedu@iet-gibb.ch>
2019-11-07 15:06:18 +0100

    a bit more responsive also added the homepage text

--- frontend/src/components/Homepage/Home.js
+++ frontend/src/components/Homepage/Home.js
@@ -7,15 +7,16 @@ function Home() {
     return (
         <div className="container">
             <div className="d-flex justify-content-center m-title">
-                <img src={logo} className="Home-logo" alt="logo" />
+                <img src={logo} className="Home-logo " alt="logo" />
                 <h1 className="font-weight-bold" id="hometitle">vitaelize</h1>
             </div>
-            <div>
+            <div className="row">
                 {/*row and justify-center*/}
-                <div className="float-left" id="div-hcv">
+
+                <div className="col-md-5 d-flex justify-content-center ml-auto" id="div-hcv">
                     <img src={cv} className="Home-cv" alt="cv" />
                 </div>
-                <div>
+                <div className="col-md-5 justify-content-center mr-auto" id="div-htext">
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </p>
