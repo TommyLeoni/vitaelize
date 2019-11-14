c9d6dd4a8d895aaa77240e465dd36225a7cd1a7b
Michel Rohrbach <michelrohrbach@Michels-MacBook-Pro.local>
2019-11-14 11:28:43 +0100

    Started to make form

---
+++ frontend/src/components/Templates/Button.js
@@ -0,0 +1,9 @@
+import './Template.css';
+import React from 'react';
+import cv from "../../images/resume.svg"
+
+function Button() {
+  return(
+    <button type="button" className="btn btn-outline-green"><img src={cv} class="select-cv"/></button>
+  )}
+  export default Button;
--- frontend/src/components/Templates/Template.js
+++ frontend/src/components/Templates/Template.js
@@ -1,53 +1,66 @@
 import './Template.css';
 import React from 'react';
 import cv from "../../images/resume.svg"
+import { Formik, Field, Form } from "formik";
+import {Button as MaterialButton} from "@material-ui/core";
+import Button from "./Button.js";
 
 function Template() {
   return(
-  <div className="container">
+    <Formik initialValues={{
+      cv: '', lang: "", color: ""}}
+      onSubmit={(data, {setSubmitting})=> {
+        setSubmitting(true);
+        console.log("submit: ", data);
+        setSubmitting(false);
+      }}
+      >
+      {({ values, isSubmitting, handleChange, handleBlur, handleSubmit}) => (
+      <Form onSubmit={handleSubmit}>
+      <div className="d-flex justify-content-center mt-5">
+        <div className="row mt-5">
+          <div class="col-sm">
+            <Field name="cv" type="radio" value="cv1" as={Button}></Field>
+            </div>
+            <div className="col-sm">
+              <Field name="cv" type="radio" value="cv2" as={Button}></Field>
+            </div>
+            <div className="col-sm">
+              <Field name="cv" type="radio" value="cv3" as={Button}></Field>
+            </div>
+          </div>
+        </div>
 
-    <div className="d-flex justify-content-center mt-5">
-      <div className="row mt-5">
-        <div class="col-sm">
-          <button type="button" className="btn btn-outline-green"><img src={cv} class="select-cv"/></button>
-        </div>
-        <div className="col-sm">
-          <button type="button" className="btn btn-outline-green"><img src={cv} class="select-cv"/></button>
-        </div>
-        <div className="col-sm">
-          <button type="button" className="btn btn-outline-green"><img src={cv} class="select-cv"/></button>
+      <div className="d-flex justify-content-center mt-3">
+        <div className="row mt-5">
+          <p className="h4">Language: </p>
+          <select className="ml-2">
+            <option value="german">German</option>
+            <option value="english">English</option>
+            <option value="french">Baguette</option>
+          </select>
         </div>
       </div>
-    </div>
 
-    <div className="d-flex justify-content-center mt-3">
-      <div className="row mt-5">
-      <p className="h4">Language: </p>
-        <select className="ml-2">
-          <option value="german">German</option>
-          <option value="english">English</option>
-          <option value="french">Baguette</option>
-        </select>
-      </div>
-    </div>
-
-    <div className="d-flex justify-content-center mt-3">
-      <div className="row mt-5">
-        <div className="col-sm">
-          <span className="h4">Color: </span>
-          <button type="button" className="btn btn-purple btn-cirlce btn-lg"></button>
-          <button type="button" className="btn btn-blue btn-cirlce btn-lg"> </button>
-          <button type="button" className="btn btn-green btn-cirlce btn-lg"></button>
-          <button type="button" className="btn btn-white btn-cirlce btn-lg"></button>
+      <div className="d-flex justify-content-center mt-3">
+        <div className="row mt-5">
+          <div className="col-sm">
+            <span className="h4">Color: </span>
+            <input type="button" className="btn btn-purple btn-circle btn-lg"></input>
+            <input type="button" className="btn btn-blue btn-circle btn-lg"></input>
+            <input type="button" className="btn btn-green btn-circle btn-lg"></input>
+            <input type="button" className="btn btn-white btn-circle btn-lg"></input>
+          </div>
         </div>
       </div>
-    </div>
-    <div className="d-flex justify-content-center mt-3">
-      <div className="row mt-5">
-        <button type="button" className="btn btn-success btn-lg">Start editing</button>
+      <div className="d-flex justify-content-center mt-3">
+        <div className="row mt-5">
+        <MaterialButton disabled={isSubmitting} type="submit">submit</MaterialButton>
+          {/*<input type="submit" className="btn btn-success btn-lg" disabled={isSubmitting } value="start editing"></input> */}
+        </div>
       </div>
-    </div>
-  </div>
+      </Form>
+    )}</Formik>
 )
 }
 export default Template;
