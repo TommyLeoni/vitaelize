b9f8ad6ce074b4475950b4d486f0f2ee90f98d7d
Michel Rohrbach <michelrohrbach@Michels-MacBook-Pro.local>
2019-11-20 17:01:11 +0100

    form is set up and can handle inputs

--- frontend/src/components/Templates/Template.js
+++ frontend/src/components/Templates/Template.js
@@ -3,24 +3,45 @@ import React from 'react';
 import cv from "../../images/resume.svg";
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
-const defaultOption = options[0];
-const options = [
-  'one', 'two', 'three'
-]
+const state = { cv: 0,
+  value: "",
+  color: ""};
+
+  function handleCv(event){
+    state.cv = event;
+    console.log(state.cv);
+
+  }
+  function logAll(){
+    console.log(state.cv, state.value, state.color);
+  }
+  function handleChange(e) {
+  let {name, value} = e.target;
+  state.value = value;
+  console.log(state.value);
+  }
+  function handleColor(event){
+    state.color = event;
+  }
 
 function Template() {
   return(
     <div>
+    <form>
+    <input type="text"/>
       <div className="d-flex justify-content-center mt-5">
         <div className="row mt-5">
           <div class="col-sm">
-          <button type="button" className="btn btn-outline-green"><img src={cv} class="select-cv"/></button>
+          <input type="radio" id="hcv1" class="input-hidden"/>
+            <label for="hcv1">
+            <button type="button" name="color" onClick={() => handleCv(1)} className="btn btn-outline-green"><img src={cv} class="select-cv"/></button>
+            </label>
           </div>
           <div className="col-sm">
-          <button type="button" className="btn btn-outline-green"><img src={cv} class="select-cv"/></button>
+          <button type="button" name="color" onclick="handleCv(2)" onClick={() => handleCv(2)} className="btn btn-outline-green"><img src={cv} class="select-cv"/></button>
           </div>
           <div className="col-sm">
-          <button type="button" className="btn btn-outline-green"><img src={cv} class="select-cv"/></button>
+          <button type="button" name="color" onclick="handleCv(3)" onClick={() => handleCv(3)} className="btn btn-outline-green"><img src={cv} class="select-cv"/></button>
           </div>
         </div>
       </div>
@@ -28,7 +49,7 @@ function Template() {
       <div className="d-flex justify-content-center mt-3">
         <div className="row mt-5">
           <p className="h4">Language: </p>
-          <select className="ml-2">
+          <select className="ml-2" onChange={handleChange}>
             <option value="german">German</option>
             <option value="english">English</option>
             <option value="french">Baguette</option>
@@ -40,19 +61,21 @@ function Template() {
         <div className="row mt-5">
           <div className="col-sm">
             <span className="h4">Color: </span>
-            <input type="button" className="btn btn-purple btn-circle btn-lg"></input>
-            <input type="button" className="btn btn-blue btn-circle btn-lg"></input>
-            <input type="button" className="btn btn-green btn-circle btn-lg"></input>
-            <input type="button" className="btn btn-white btn-circle btn-lg"></input>
+            <input type="button" onClick={() => handleColor("purple")} className="btn btn-purple btn-circle btn-lg"></input>
+            <input type="button" onClick={() => handleColor("blue")} className="btn btn-blue btn-circle btn-lg"></input>
+            <input type="button" onClick={() => handleColor("green")} className="btn btn-green btn-circle btn-lg"></input>
+            <input type="button" onClick={() => handleColor("white")} className="btn btn-white btn-circle btn-lg"></input>
+
           </div>
         </div>
       </div>
 
       <div className="d-flex justify-content-center mt-3">
         <div className="row mt-5">
-          <input type="submit" className="btn btn-success btn-lg" value="start editing"></input>
+          <input type="button" className="btn btn-success btn-lg" value="start editing" onClick={logAll}></input>
         </div>
       </div>
+      </form>
       </div>
 )
 }
