ad63d8086913530aa43176017bbe8f8fa477b90f
Administrator adm-virtedu <adm-virtedu@iet-gibb.ch>
2019-12-05 10:36:26 +0100

    test push

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,17 +8,14 @@ router.post("/register", async (req, res) => {
   //Validate data before instantiating new user model
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-  console.log("data vaildated")
 
   //Check if user already in database
   const emailExist = await User.findOne({ email: req.body.email });
   if (emailExist) return res.status(400).send("Email already taken!");
-  console.log("user checked")
 
   //Hash password
   const salt = await bcrypt.genSalt(10);
   const hashPassword = await bcrypt.hash(req.body.password, salt);
-  console.log("pw hashed");
 
   //Create a new user
   const user = new User({
@@ -26,10 +23,12 @@ router.post("/register", async (req, res) => {
     email: req.body.email,
     password: hashPassword
   });
-  console.log("user created");
+
   try {
     const savedUser = await user.save();
-    res.send({ user: savedUser._id });
+    const token = jwt.sign({ _id: savedUser._id }, process.env.TOKEN_SECRET);
+    res.header("auth-token", token).send(token);
+
   } catch (err) {
     res.status(400).send(err);
   }
