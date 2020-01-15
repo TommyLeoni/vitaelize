195d84536af398b6893406522f71799de7b70ae4
Tomaso Leoni <tomasoleoni011@gmail.com>
2020-01-15 20:04:47 +0100

    working on further templates

--- backend/routes/pdfRoutes.js
+++ backend/routes/pdfRoutes.js
@@ -2,15 +2,20 @@ const router = require("express").Router();
 const verify = require("./verifyToken");
 const generator = require("../templating/generator");
 const pdfController = require("../pdf");
+const fs = require("fs");
 
 router.post("/generate", verify, async (req, res) => {
   try {
-    //const pdfSettings = req.body.pdfSettings;
-    //pdfController(generator.first(pdfSettings));
-    res.send({msg: req.files.portrait.name})
-    res.send({msg: "Your PDF was successfully created and sent to your email!"})
+    var pic = req.files.portrait;
+    const pdfSettings = req.body.pdfSettings;
+
+    pic.mv(`templating/tmp/${pic.name}`, function(err) {
+      if (err) return res.status(500).send(err);
+      pdfController(generator.first(pdfSettings, pic.name));
+      res.send({ msg: pic.name });
+    });
   } catch (error) {
-      console.log(error);
+    console.log(error);
     res.status(500).send("An error occurred while creating your pdf.");
   }
 });
